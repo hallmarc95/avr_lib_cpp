38c4103c4f8cded1ff255be9844630db3281dcfa
hallmarc95 <48543283+hallmarc95@users.noreply.github.com>
2020-09-12 17:32:35 -0700

    Updated code to reflect changes to AVRIO namespace changes.

--- Blink/main.cpp
+++ Blink/main.cpp
@@ -6,15 +6,15 @@
 
 
 void initIO() {
-    AVRIO::DDRB = (1 << AVRIO::PB5);
-    AVRIO::PORTB = 0x00;
+    AVRIO::DDRB = (1 << PB5);
+    AVRIO::PORTB = AVRIO::PortB(0x00);
 }
 
 int main() {
 	initIO();
 
 	while (1) {
-        AVRIO::DDRB ^= (1 << AVRIO::PB5);
+        AVRIO::DDRB ^= (1 << PB5);
         _delay_ms(100);
 	}
 	return 0; // never reached
